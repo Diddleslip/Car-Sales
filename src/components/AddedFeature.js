a23a566879299293071384a24809b6e49ccc4684
Diddleslip <diddleslip@gmail.com>
2020-04-15 22:33:49 -0400

    Finished MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { removeFeature } from "../store/actions";
+import { connect } from "react-redux"
 
 const AddedFeature = props => {
+  const removeItem = item => {
+    // dispatch an action here to remove an item
+    props.removeFeature(item);
+  }
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={() => removeItem(props.feature)}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(null, { removeFeature })(AddedFeature);
---
+++ src/store/actions/index.js
@@ -0,0 +1,11 @@
+import { ADD_FEATURE, 
+         REMOVE_FEATURE, 
+         addFeature, 
+         removeFeature 
+} from "./carAction";
+
+export { ADD_FEATURE, 
+         REMOVE_FEATURE, 
+         addFeature, 
+         removeFeature 
+}
